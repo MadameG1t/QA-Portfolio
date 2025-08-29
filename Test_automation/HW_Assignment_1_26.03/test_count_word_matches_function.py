69d5e7cc006bf4faefe7e773c43fa4ddeafa9f72
Gretchen Schadebrodt <gschadebrodtz@gmail.com>
2025-08-29 15:31:26 +0200

    Test cases for Exercise 1: Basic Parameterized Tests

--- Test_automation/HW_Assignment_1_26.03/test_count_word_matches_function.py
+++ Test_automation/HW_Assignment_1_26.03/test_count_word_matches_function.py
@@ -1,5 +1,22 @@
 import count_word_matches_function
 
-
 def test_count_word_matches_function():
     assert count_word_matches_function.count_word_matches(text="The cat sat on the mat", target="cat") == 1
+
+def test_count_word_matches_function():
+    assert count_word_matches_function.count_word_matches(text="Dog dog DOG dOg", target="dog") == 4
+
+def test_count_word_matches_function():
+    assert count_word_matches_function.count_word_matches(text="Hello world", target="world") == 1
+
+def test_count_word_matches_function():
+    assert count_word_matches_function.count_word_matches(text="hello hello HELLO", target="hello") == 3
+
+def test_count_word_matches_function():
+    assert count_word_matches_function.count_word_matches(text="No matches here", target="yes" ) == 0
+
+def test_count_word_matches_function():
+    assert count_word_matches_function.count_word_matches(text="catcat cat catdog", target="cat" ) == 1
+
+def test_count_word_matches_function():
+    assert count_word_matches_function.count_word_matches(text="a a a", target="a") == 3
