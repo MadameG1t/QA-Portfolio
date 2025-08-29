800ac3b141dd277ddf75ce25744c3657fef469e5
Gretchen Schadebrodt <gschadebrodtz@gmail.com>
2025-08-29 17:07:41 +0200

    organized filed updated. Previous functions deleted. No need to have a function per test case.

--- Test_automation/HW_Assignment_1_26.03/test_count_word_matches_function.py
+++ Test_automation/HW_Assignment_1_26.03/test_count_word_matches_function.py
@@ -15,26 +15,5 @@ import pytest
 def test_count_word_matches_function_1(text,target,expected):
     assert count_word_matches_function.count_word_matches(text,target) == expected
 
-'''
-def test_count_word_matches_function1():
-    assert count_word_matches_function.count_word_matches(text="The cat sat on the mat", target="cat") == 1
 
-def test_count_word_matches_function2():
-    assert count_word_matches_function.count_word_matches(text="Dog dog DOG dOg", target="dog") == 4
 
-def test_count_word_matches_function3():
-    assert count_word_matches_function.count_word_matches(text="Hello world", target="world") == 1
-
-def test_count_word_matches_function4():
-    assert count_word_matches_function.count_word_matches(text="hello hello HELLO", target="hello") == 3
-
-def test_count_word_matches_function5():
-    assert count_word_matches_function.count_word_matches(text="No matches here", target="yes" ) == 0
-
-def test_count_word_matches_function6():
-    assert count_word_matches_function.count_word_matches(text="catcat cat catdog", target="cat" ) == 1
-
-def test_count_word_matches_function7():
-    assert count_word_matches_function.count_word_matches(text="a a a", target="a") == 3
-
-'''
