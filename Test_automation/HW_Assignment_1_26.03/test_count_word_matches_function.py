facaf243237ea468a8fadb244e901c323a34731e
Gretchen Schadebrodt <gschadebrodtz@gmail.com>
2025-08-30 13:32:44 +0200

    doing the third assignment on negative testing.

--- Test_automation/HW_Assignment_1_26.03/test_count_word_matches_function.py
+++ Test_automation/HW_Assignment_1_26.03/test_count_word_matches_function.py
@@ -12,8 +12,27 @@ import pytest
     ("a a a","a",3)
 ])
 
+
 def test_count_word_matches_function_1(text,target,expected):
     assert count_word_matches_function.count_word_matches(text,target) == expected
 
+@pytest.fixture(scope = "function")
+def sample_data():
+    return [("None","word",TypeError),
+            ("hello world","None",TypeError),
+            (123,"word",TypeError),
+            ("hello world",456,TypeError),
+            (["hello", "world"],"world",TypeError),
+            ("hello world",["world"],TypeError)
+            ]
 
 
+def test_input_none(sample_data):
+    with pytest.raises(TypeError,match == "Please insert a string value"):
+    assert count_word_matches_function.count_word_matches(sample_data) == expected
+
+def test_input_integer(sample_data):
+    assert count_word_matches_function.count_word_matches(sample_data) == expected
+
+def test_input_list(sample_data):
+    assert count_word_matches_function.count_word_matches(sample_data) == expected
