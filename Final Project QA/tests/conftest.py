2ab0bdad92148292522ad15f1e2a52212bbda6c2
Gretchen Schadebrodt <gschadebrodtz@gmail.com>
2026-01-25 17:40:43 +0100

    doing the third assignment on negative testing.

---
+++ Final Project QA/tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+@pytest.fixture()
+def driver():
+    options = Options()
+    options.add_argument("--start-maximized")
+    drv = webdriver.Chrome(options=options)
+    yield drv
+    drv.quit()
