9b26549a2cf60630cf17e628bb9cfc6d2cc53a6d
Gretchen Schadebrodt <gschadebrodtz@gmail.com>
2026-01-25 16:55:07 +0100

    doing the third assignment on negative testing.

--- Final Project QA/pages/age_gate_page.py
+++ Final Project QA/pages/age_gate_page.py
@@ -5,5 +5,11 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class AgeGatePage:
     DOB_INPUT = (By.CSS_SELECTOR, "input[placeholder='DD-MM-YYYY']")
-    SUBMIT_BTN = (By.XPATH, "//button")
+    SUBMIT_BTN = (By.XPATH, "//button[normalize-space()='confirm']")
 
+def __init__(self, driver, timeout=10):
+    self.driver = driver
+    self.wait = WebDriverWait(driver, timeout)
+
+def open(self, url: str):
+    self.driver.get(url)
