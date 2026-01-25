6e822d79aec48d88323de5cccb5f1d977c94f661
Gretchen Schadebrodt <gschadebrodtz@gmail.com>
2026-01-25 17:27:13 +0100

    doing the third assignment on negative testing.

--- Final Project QA/tests/test_age_restriction.py
+++ Final Project QA/tests/test_age_restriction.py
@@ -1,5 +1,25 @@
 from datetime import date
 
 from pages.age_gate_page import AgeGatePage
-from utils.constants import Urls, ARgeules
+from utils.constants import Urls, AgeRules
 from utils.helpers import date_of_birth_for_age_years, date_of_birth_entry_format
+
+
+def test_user_exactly_18_can_pass_age_gate(driver):
+    page = AgeGatePage(driver)
+
+
+def test_user_just_below_18_cannot_pass_age_gate(driver):
+    pass
+
+def test_user_below_17_cannot_pass_age_gate(driver):
+    pass
+
+def test_user_above_18_can_pass_age_gate(driver):
+    pass
+
+def test_user_dob_not_entered_cannot_pass_age_gate(driver):
+    pass
+
+def test_user_invalid_dob_cannot_pass_age_gate(driver):
+    pass
