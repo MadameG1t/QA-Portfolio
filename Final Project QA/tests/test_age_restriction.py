08a6371b66f0edc4a05e89a54ac0c4b04cce661a
Gretchen Schadebrodt <gschadebrodtz@gmail.com>
2026-01-25 17:17:42 +0100

    doing the third assignment on negative testing.

---
+++ Final Project QA/tests/test_age_restriction.py
@@ -0,0 +1,5 @@
+from datetime import date
+
+from pages.age_gate_page import AgeGatePage
+from utils.constants import Urls, ARgeules
+from utils.helpers import date_of_birth_for_age_years, date_of_birth_entry_format
