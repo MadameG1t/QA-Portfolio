4bb816b0140340e10e19018c1b3bb49914ffeb65
Gretchen Schadebrodt <gschadebrodtz@gmail.com>
2026-01-25 18:00:10 +0100

    doing the third assignment on negative testing.

--- Final Project QA/tests/test_age_restriction.py
+++ Final Project QA/tests/test_age_restriction.py
@@ -1,4 +1,5 @@
 from datetime import date
+import time
 
 from pages.age_gate_page import AgeGatePage
 from utils.constants import Urls, AgeRules
@@ -8,11 +9,15 @@ from utils.helpers import date_of_birth_for_age_years, date_of_birth_entry_forma
 def test_user_exactly_18_can_pass_age_gate(driver):
     page = AgeGatePage(driver)
     page.open(Urls.HOME)
+    time.sleep(3)
     page.go_to_store()
+    time.sleep(3)
     dob_date = date_of_birth_for_age_years(date.today(), AgeRules.MIN_AGE)
     dob_str = date_of_birth_entry_format(dob_date, AgeRules.DOB_FORMAT_HINT)
     page.enter_dob(dob_str)
+    time.sleep(3)
     page.submit()
+    time.sleep(0)
 
     assert page.age_gate_closed(),\
     "Expected age verification popup to close after entering valid DOB,but stayed open"
