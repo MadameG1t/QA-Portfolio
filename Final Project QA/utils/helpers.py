7a8175e43eb631b439862cd2aebe4a75564247ab
Gretchen Schadebrodt <gschadebrodtz@gmail.com>
2026-01-25 15:46:26 +0100

    doing the third assignment on negative testing.

---
+++ Final Project QA/utils/helpers.py
@@ -0,0 +1,20 @@
+from datetime import date
+
+
+def date_of_birth_for_age_years(today: date,years: int):
+
+    try:
+        return today.replace(year=today.year - years)
+    except ValueError:
+        return today.replace(month=2,day=28,year=today.year - years)
+
+def date_of_birth_entry_format(d:date,fmt: str = "DD-MM-YYYY"):
+
+    if fmt == "DD-MM-YYYY":
+        return d.strftime("%d-%m-%Y")
+    if fmt == "YYYY-MM-DD":
+        return d.strftime("%Y-%m-%d")
+    if fmt == "MM/DD/YYYY":
+        return d.strftime("%m/%d/%Y")
+
+    return d.strftime("%d-%m-%Y")
