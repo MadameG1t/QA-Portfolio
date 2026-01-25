0890f5c573113b3829c7e67e4f6858c39914c19d
Gretchen Schadebrodt <gschadebrodtz@gmail.com>
2026-01-25 17:37:39 +0100

    doing the third assignment on negative testing.

--- Final Project QA/tests/test_age_restriction.py
+++ Final Project QA/tests/test_age_restriction.py
@@ -7,6 +7,13 @@ from utils.helpers import date_of_birth_for_age_years, date_of_birth_entry_forma
 
 def test_user_exactly_18_can_pass_age_gate(driver):
     page = AgeGatePage(driver)
+    page.open(Urls.HOME)
+    dob_date = date_of_birth_for_age_years(date.today(), AgeRules.MIN_AGE)
+    dob_str = date_of_birth_entry_format(dob_date, AgeRules.DOB_FORMAT_HINT)
+    page.enter_dob(dob_str)
+    page.submit()
+
+    assert page.driver.current_url != Urls.HOME
 
 
 def test_user_just_below_18_cannot_pass_age_gate(driver):
