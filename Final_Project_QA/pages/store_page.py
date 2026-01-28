d25c0857bca4cb1d2d4ea3eb3dd42332ba7dc36d
Gretchen Schadebrodt <gschadebrodtz@gmail.com>
2026-01-28 19:13:56 +0100

    doing the third assignment on negative testing.

---
+++ Final_Project_QA/pages/store_page.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class StorePage:
+    PRODUCT_CARDS = (By.CSS_SELECTOR, "div.product-grid div.product-card")
+    ADD_TO_CART_IN_CARD = (By.CSS_SELECTOR, "button.btn-cart")
+
+    def __init__(self, driver: WebDriver, timeout: int = 10):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, timeout)
+
+    def add_first_product_to_cart(self) -> None:
+        cards = self.wait.until(EC.presence_of_all_elements_located(self.PRODUCT_CARDS))
+        first_card = cards[0]
+
+        add_btn = first_card.find_element(*self.ADD_TO_CART_IN_CARD)
+        self.wait.until(EC.element_to_be_clickable(add_btn)).click()
