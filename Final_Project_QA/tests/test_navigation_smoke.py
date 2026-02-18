924edd852f991a275d5b5dc4d078a546c1e02527
Gretchen Schadebrodt <gschadebrodtz@gmail.com>
2026-02-18 17:56:46 +0100

    doing the third assignment on negative testing.

--- Final_Project_QA/tests/test_navigation_smoke.py
+++ Final_Project_QA/tests/test_navigation_smoke.py
@@ -7,16 +7,11 @@ from pages.store_page import StorePage
 from pages.cart_page import CartPage
 
 
-def test_can_open_cart_checkout_page(driver):
-    driver.get(Urls.STORE)
-    AgeGatePage(driver).pass_age_gate_if_present()
 
-    StorePage(driver).add_first_product_to_cart_with_quantity(1)
-
+def test_can_open_cart_checkout_page(driver, purchased_product):
     driver.get(Urls.CHECKOUT)
-
-    assert "/checkout" in driver.current_url
 
     WebDriverWait(driver, 10).until(
         EC.visibility_of_element_located(CartPage.CHECKOUT_CARD)
     )
+    assert "/checkout" in driver.current_url
