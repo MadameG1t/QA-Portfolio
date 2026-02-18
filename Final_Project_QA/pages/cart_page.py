ce4478356f55b7293caf501bc97d65f2692b4bf6
Gretchen Schadebrodt <gschadebrodtz@gmail.com>
2026-02-18 11:13:39 +0100

    doing the third assignment on negative testing.

--- Final_Project_QA/pages/cart_page.py
+++ Final_Project_QA/pages/cart_page.py
@@ -27,7 +27,7 @@ class CartPage:
         self.wait = WebDriverWait(driver, timeout)
 
     def open_cart_via_icon(self):
-        icon = self.wait.until(EC.visibility_of_element_located(self.CART_ICON))
+        icon = self.wait.until(EC.element_to_be_clickable(self.CART_ICON))
         ActionChains(self.driver).move_to_element(icon).pause(0.2).click(icon).perform()
         self.wait.until(EC.visibility_of_element_located(self.CHECKOUT_CARD))
 
