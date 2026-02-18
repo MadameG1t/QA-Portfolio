a00b3ecaaebe01492e66691d222f62393e83fc35
Gretchen Schadebrodt <gschadebrodtz@gmail.com>
2026-02-18 18:02:10 +0100

    doing the third assignment on negative testing.

--- Final_Project_QA/tests/test_shipping_cost.py
+++ Final_Project_QA/tests/test_shipping_cost.py
@@ -1,11 +1,12 @@
 import pytest
-
 from pages.cart_page import CartPage
 
 
 @pytest.fixture
-def cart_page(cart_ready):
-    return CartPage(cart_ready)
+def cart_page(driver, purchased_product):
+    return CartPage(driver)
+
 
 def test_shipping_cost_is_5_when_total_below_20(cart_page):
-    assert cart_page.get_shipment() == 5.0
+    shipment = cart_page.get_shipment()
+    assert shipment == 5.0, f"Expected shipment to be 5€, but got {shipment}"
