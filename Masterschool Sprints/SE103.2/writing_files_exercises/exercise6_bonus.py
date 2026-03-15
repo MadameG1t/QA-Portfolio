648e51f8ca06a598064b47609997004fb62ad9a1
Gretchen Schadebrodt <gschadebrodtz@gmail.com>
2026-03-15 16:35:06 +0100

    add_print_statement doing the third assignment on negative testing.

--- Masterschool Sprints/SE103.2/writing_files_exercises/exercise6_bonus.py
+++ Masterschool Sprints/SE103.2/writing_files_exercises/exercise6_bonus.py
@@ -1,20 +1,14 @@
 import csv
-import os
-
-print(os.getcwd())
 
 with open("olympic-medals.csv", "r", newline="") as infile:
     reader = csv.reader(infile)
-    next(reader)
+    next(reader)  # skip header
 
     for row in reader:
-        if len(row) < 5:
-            continue
-
-        country = row[1].strip()
-        gold = int(row[2])
-        silver = int(row[3])
-        bronze = int(row[4])
+        country = row[0]
+        gold = int(row[1])
+        silver = int(row[2])
+        bronze = int(row[3])
 
         total = gold + silver + bronze
 
